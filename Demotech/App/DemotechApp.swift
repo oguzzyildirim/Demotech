ccd9a565c1d4233dca6a5bb62458fb07e384446e
Oğuz Yıldırım <oguzyildirimyo@icloud.com>
2026-09-09 01:33:47 +0300

    first commit

---
+++ Demotech/App/DemotechApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemotechApp.swift
+//  Demotech
+//
+//  Created by Oguz Yildirim on 9.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct DemotechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demotech/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Demotech
+//
+//  Created by Oguz Yildirim on 9.09.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
